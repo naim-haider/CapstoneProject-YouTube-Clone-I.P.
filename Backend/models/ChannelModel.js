8eaad506a95f53cebfa2f7f5246891a2dc88c2bf
naim-haider <naimhaider76@gmail.com>
2025-02-12 23:40:00 +0530

    reInstalled tailwind in frontend and created userModel, channelModel, videoModel, and commentModel

---
+++ Backend/models/ChannelModel.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const channelSchema = new mongoose.Schema(
+  {
+    channelName: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    channelBanner: {
+      type: String,
+      default:
+        "https://static.vecteezy.com/system/resources/previews/009/292/244/original/default-avatar-icon-of-social-media-user-vector.jpg",
+    },
+    subscribers: {
+      type: Number,
+      default: 0,
+    },
+    videos: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Channel = mongoose.model("Channel", channelSchema);
+export default Channel;
---
+++ Backend/models/CommentModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    videoId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    timestamp: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+const Comment = mongoose.model("Comment", commentSchema);
+export default Comment;
